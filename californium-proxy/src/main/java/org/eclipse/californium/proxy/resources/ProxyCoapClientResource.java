85bfcdf8c36d5b6069cb9c3bf9e2659edd6e6cb8
smartjune <yuanzhongyan@gmail.com>
2016-01-06 10:48:36 +0800

    adjusted to ProxyWebsocketServer, removing translations

--- californium-proxy/src/main/java/org/eclipse/californium/proxy/resources/ProxyCoapClientResource.java
+++ californium-proxy/src/main/java/org/eclipse/californium/proxy/resources/ProxyCoapClientResource.java
@@ -42,59 +42,59 @@ public class ProxyCoapClientResource extends ForwardingResource {
 
 	@Override
 	public Response forwardRequest(Request request) {
-		LOGGER.info("ProxyCoAP2CoAP forwards "+request);
-		Request incomingRequest = request;
+		LOGGER.info("ProxyCoAP2CoAP forwards \n\t"+request);
+		//Request incomingRequest = request;
 
 		// check the invariant: the request must have the proxy-uri set
-		if (!incomingRequest.getOptions().hasProxyUri()) {
+/*		if (!incomingRequest.getOptions().hasProxyUri()) {
 			LOGGER.warning("Proxy-uri option not set.");
 			return new Response(ResponseCode.BAD_OPTION);
 		}
-
+*/
 		// remove the fake uri-path
 		// FIXME: HACK // TODO: why? still necessary in new Cf?
-		incomingRequest.getOptions().clearUriPath();
+//		incomingRequest.getOptions().clearUriPath();
 
 		// create a new request to forward to the requested coap server
-		Request outgoingRequest = null;
-		try {
+		Request outgoingRequest = request;
+/*	try {
 			// create the new request from the original
 			outgoingRequest = CoapTranslator.getRequest(incomingRequest);
+			
+			LOGGER.finer("\t翻译前的coap请求：" + incomingRequest + "\n\t翻译后的coap请求" + outgoingRequest);
 
-//			// enable response queue for blocking I/O
-//			outgoingRequest.enableResponseQueue(true);
-
-			// get the token from the manager // TODO: necessary?
-//			outgoingRequest.setToken(TokenManager.getInstance().acquireToken());
-
-			// execute the request
-			LOGGER.finer("Sending coap request.");
-//			outgoingRequest.execute();
-			LOGGER.info("ProxyCoapClient received CoAP request and sends a copy to CoAP target");
-			outgoingRequest.send();
-
-			// accept the request sending a separate response to avoid the
-			// timeout in the requesting client
-			LOGGER.finer("Acknowledge message sent");
 		} catch (TranslationException e) {
 			LOGGER.warning("Proxy-uri option malformed: " + e.getMessage());
 			return new Response(CoapTranslator.STATUS_FIELD_MALFORMED);
 		} catch (Exception e) {
 			LOGGER.warning("Failed to execute request: " + e.getMessage());
 			return new Response(ResponseCode.INTERNAL_SERVER_ERROR);
-		}
+		}*/
 
+		LOGGER.info("ProxyCoapClient received CoAP request and sends a copy to CoAP target");
+		LOGGER.finer("Sending coap request.");
+		outgoingRequest.setURI(outgoingRequest.getURI());
+		outgoingRequest.send();
+
+		// accept the request sending a separate response to avoid the
+		// timeout in the requesting client
+		LOGGER.finer("Acknowledge message sent");
+
+		
 		try {
 			// receive the response // TODO: don't wait for ever
 			Response receivedResponse = outgoingRequest.waitForResponse();
 
 			if (receivedResponse != null) {
-				LOGGER.finer("Coap response received.");
+/*				LOGGER.finer("Coap response received.");
 
 				// create the real response for the original request
 				Response outgoingResponse = CoapTranslator.getResponse(receivedResponse);
 
-				return outgoingResponse;
+				LOGGER.finer("\t翻译前的coap响应：" + receivedResponse + "\n\t翻译后的coap响应" + outgoingResponse);
+				
+				return outgoingResponse;*/
+				return receivedResponse;
 			} else {
 				LOGGER.warning("No response received.");
 				return new Response(CoapTranslator.STATUS_TIMEOUT);
